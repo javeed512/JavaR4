45b7ec6a328c611f22455c90d1048ef1fb3b4a0b
javeed512 <javeedmohammedjaveed@gmail.com>
2021-03-04 15:50:09 +0530

    login module by pranav

---
+++ myapplication/src/com/capg/app/Login.java
@@ -0,0 +1,13 @@
+package com.capg.app;
+
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Login by pranav");
+		
+	}
+
+}
