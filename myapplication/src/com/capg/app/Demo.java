fc1bbb09c00331852f42d65caa13ddf0fc42a893
Admin <Admin@DESKTOP-7JB657P>
2021-03-04 12:47:21 +0530

    commit by javeed

---
+++ myapplication/src/com/capg/app/Demo.java
@@ -0,0 +1,15 @@
+package com.capg.app;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello friends");
+		
+		System.out.println("Code modified");
+		
+		System.out.println("hii guys");
+
+	}
+
+}
